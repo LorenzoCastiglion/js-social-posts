9fd125954e1ae9d8b0f799d599f4b07f71c23013
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-10-24 18:19:25 +0200

    bonus data

--- JS/main.js
+++ JS/main.js
@@ -81,6 +81,22 @@ const posts = [
 ];
 
 
+
+
+// per prendere la data in formato DD/MM/YYYY
+
+const today = new Date();
+const yyyy = today.getFullYear();
+let mm = today.getMonth() + 1; 
+let dd = today.getDate();
+
+if (dd < 10) dd = '0' + dd;
+if (mm < 10) mm = '0' + mm;
+
+const formattedToday = dd + '/' + mm + '/' + yyyy;
+
+
+
 let container = document.getElementById('container')
 
 
@@ -100,12 +116,12 @@ posts.forEach(element => {
                 <img class="profile-pic" src="${element.author.image}" alt="${element.name}">                    
             </div>
             <div class="post-meta__data">
-                <div class="post-meta__author">"${element.author.name}"</div>
-                <div class="post-meta__time">dd/mm/yyyy</div>
+                <div class="post-meta__author">${element.author.name}</div>
+                <div class="post-meta__time">${formattedToday}</div>
             </div>                    
         </div>
     </div>
-    <div class="post__text">"${element.content}"
+    <div class="post__text">${element.content}
     </div>
     <div class="post__image">
         <img src="${element.media}" alt="">
